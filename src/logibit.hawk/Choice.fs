b149fd5b7810bb66f6b4b874d64ecdbb148e8bb6
Henrik Feldt <henrik@haf.se>
2014-12-30 15:13:37 +0100

    [Choice] exposing Choice combinators

---
+++ src/logibit.hawk/Choice.fs
@@ -0,0 +1,52 @@
+﻿module logibit.hawk.Choice
+
+let of_option on_error = function
+  | Some x -> Choice1Of2 x
+  | None   -> Choice2Of2 on_error
+
+let (>>=) m f =
+  m
+  |> function
+  | Choice1Of2 x   -> f x
+  | Choice2Of2 err -> Choice2Of2 err
+
+/// bind the successful value to f
+let bind f m = (m >>= f)
+
+/// bind f to the successful value
+let (>>!) m f =
+  m
+  |> function
+  | Choice1Of2 x -> Choice1Of2 x
+  | Choice2Of2 err -> f err
+
+/// bind f to the error value
+let bind_2 m f =
+  m >>! f
+
+/// lift the success value
+let lift ``pure`` = Choice1Of2 ``pure``
+
+let (>>~) ``pure`` f =
+  lift ``pure`` >>= f
+
+/// lift the value and bind to f
+let lift_bind ``pure`` f = ``pure`` >>~ f
+
+let (>>-) m f =
+  m
+  |> function
+  | Choice1Of2 x   -> Choice1Of2 (f x)
+  | Choice2Of2 err -> Choice2Of2 err
+
+/// map success
+let map f o = (o >>- f)
+
+let (>>@) m f =
+  m
+  |> function
+  | Choice1Of2 x -> Choice1Of2 x
+  | Choice2Of2 err -> Choice2Of2 (f err)
+
+/// map error
+let map_2 f o = o >>@ f
--- src/logibit.hawk/Prelude.fs
+++ src/logibit.hawk/Prelude.fs
@@ -79,48 +79,6 @@ module Writer =
   let ``return`` m =
     m.state
 
-module Choice =
-
-  let of_option on_error = function
-    | Some x -> Choice1Of2 x
-    | None   -> Choice2Of2 on_error
-
-  let (>>=) m f =
-    m
-    |> function
-    | Choice1Of2 x   -> f x
-    | Choice2Of2 err -> Choice2Of2 err
-
-  /// bind the successful value to f
-  let bind f m = (m >>= f)
-
-  /// lift the success value
-  let lift ``pure`` = Choice1Of2 ``pure``
-
-  let (>>~) ``pure`` f =
-    lift ``pure`` >>= f
-
-  /// lift the value and bind to f
-  let lift_bind ``pure`` f = ``pure`` >>~ f
-
-  let (>>-) m f =
-    m
-    |> function
-    | Choice1Of2 x   -> Choice1Of2 (f x)
-    | Choice2Of2 err -> Choice2Of2 err
-
-  /// map success
-  let map f o = (o >>- f)
-
-  let (>>@) m f =
-    m
-    |> function
-    | Choice1Of2 x -> Choice1Of2 x
-    | Choice2Of2 err -> Choice2Of2 (f err)
-
-  /// map error
-  let map_2 f o = o >>@ f
-
 module Hash =
   open System.Text
   open System.Security.Cryptography
